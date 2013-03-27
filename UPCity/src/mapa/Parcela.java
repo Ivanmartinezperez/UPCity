4959c389782dda3b0b51b4dc188454dd94e5764c
Ivan Martinez <ivaninho104@gmail.com>
2013-03-27 14:16:32 +0100

    Atributos añadidos

--- UPCity/src/mapa/Parcela.java
+++ UPCity/src/mapa/Parcela.java
@@ -9,5 +9,20 @@ package mapa;
  * @author ivanmartinez
  */
 public class Parcela {
+    int cordenada_x;
+    int coordenada_y;
+    String Estado;
     
+    Parcela(int x, int i, String estado){
+        
+    }
+    
+    void setParcela(){
+        
+    }
+    
+    void getParcela(){
+        
+    }
 }
+  
