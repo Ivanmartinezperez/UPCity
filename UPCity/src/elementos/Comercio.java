d8387d3b9524691155c24e6f7fd90e2cf6ea841f
Ivan Martinez <ivaninho104@gmail.com>
2013-03-27 19:09:29 +0100

    Dos elementos implementados

--- UPCity/src/elementos/Comercio.java
+++ UPCity/src/elementos/Comercio.java
@@ -8,6 +8,22 @@ package elementos;
  *
  * @author ivanmartinez
  */
-public class Comercio {
+public class Comercio extends Elementos {
+    public int capacidad_del_comercio;
+    
+    public Comercio(int OID, int cap){
+        super(OID);
+        this.capacidad_del_comercio=cap;
+        
+    }
+    
+    public void setCapacidad(int cap){
+        this.capacidad_del_comercio=cap;
+    }
+    
+    public int getCapacidad(){
+        return capacidad_del_comercio;
+    }
+    
     
 }
--- UPCity/src/elementos/Espacio_publico.java
+++ UPCity/src/elementos/Espacio_publico.java
@@ -8,6 +8,13 @@ package elementos;
  *
  * @author ivanmartinez
  */
-public class Espacio_publico {
+public class Espacio_publico extends Elementos {
+    /*Este atributo nos indica a cuanta gente puede atender el comercio*/
+    public int tipo_espacio_publico;
+    
+    public Espacio_publico(int OID,int tipo){
+        super(OID);
+        this.tipo_espacio_publico=tipo;
+    }
     
 }
