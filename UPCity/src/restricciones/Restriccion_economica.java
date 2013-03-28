53f1cdb0fa11be30b638a5a83f95baea9fcc9587
ron <ron@ron-PC>
2013-03-28 13:18:05 +0100

    restriccion economica empezada

--- UPCity/src/restricciones/Restriccion_economica.java
+++ UPCity/src/restricciones/Restriccion_economica.java
@@ -6,8 +6,46 @@ package restricciones;
 
 /**
  *
- * @author ivanmartinez
+ * @author rondavidsofer
  */
 public class Restriccion_economica {
-    
+     
+   public int saldo;
+   public int[] saldos;
+   
+   
+   //CREADORA///////
+   public Restriccion_economica( int saldo_com, int saldo_viv, int saldo_pub){
+       this.saldo = saldo_com + saldo_viv + saldo_pub;
+       int tam = 3;
+       this.saldos = new int[tam];
+       this.saldos[0] = saldo_com;
+       this.saldos[1] = saldo_viv;
+       this.saldos[2] = saldo_pub;
+   }
+   /////////////////
+   
+   //MODIFICADORAS///////
+   public void Modificarsaldo(int saldo_com, int saldo_viv, int saldo_pub){
+       this.saldo = saldo_com + saldo_viv + saldo_pub;
+       this.saldos[0] = saldo_com;
+       this.saldos[1] = saldo_viv;
+       this.saldos[2] = saldo_pub;
+   }
+   
+   public void Modificarsaldo_indiv(int id, int saldo_ind){
+       this.saldo = this.saldo - this.saldos[id];
+       this.saldos[id] = saldo_ind;
+       this.saldo = this.saldo + saldo_ind;
+   }
+   ///////////////////////
+   
+   //CONSULTORAS/////////
+   public int consultar_saldo(){
+       return this.saldo;
+   }
+   
+   public int consultar_saldo_ind(int id){
+       return this.saldo[id];
+   }
 }
