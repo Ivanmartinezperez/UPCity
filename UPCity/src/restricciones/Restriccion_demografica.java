53a9298d87891d03bdaa7505fa154055ee2f10b9
Ivan Martinez <ivaninho104@gmail.com>
2013-03-28 21:20:21 +0100

    Prueba de override de metodo; Funciona, Cojonudo!!!

--- UPCity/src/restricciones/Restriccion_demografica.java
+++ UPCity/src/restricciones/Restriccion_demografica.java
@@ -3,11 +3,16 @@
  * and open the template in the editor.
  */
 package restricciones;
+import elementos.*;
 
 /**
  *
  * @author ivanmartinez
  */
 public class Restriccion_demografica {
+    
+    boolean checkRestriction(Cjt_Elementos ce, int prueba){
+        return true;
+    }
     
 }
