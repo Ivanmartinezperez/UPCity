aac90eb25f345843c62b8c40a047f9ba80c8880b
arclorensarth <arclorensarth@arclorensarth-VirtualBox>
2013-03-31 20:11:26 +0200

    Comienzo de controladores: Controlador de la gestion de barrios comenzado(fuck Java and his Classes jajaja)

---
+++ UPCity/src/controladors/VistaMenuMantBarrios.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controladors;
+
+/**
+ *
+ * @author arclorensarth
+ */
+public class VistaMenuMantBarrios {
+    
+    
+    public void visualizar(){
+        
+    }
+    
+    public int preguntar(String S){
+        int res = 0;
+        
+        return res;
+    }
+    
+}
