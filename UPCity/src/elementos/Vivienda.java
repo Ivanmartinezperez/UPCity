712ffaa3d5315eeaad5ef1cec5be309c9b1d94d1
Ivan Martinez <ivaninho104@gmail.com>
2013-04-11 17:58:42 +0200

    cambiado el nombre de la clase, puesto en singular

---
+++ UPCity/src/elementos/Vivienda.java
@@ -0,0 +1,55 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package elementos;
+
+/**
+ *
+ * @author ivanmartinez
+ */
+public class Vivienda extends Elemento {
+    
+    public int capacidad_maxima;
+    public int Tamano_x;
+    public int Tamano_y;
+    public int Precio;
+    
+    public Vivienda(int OID){
+        super(OID);
+    }
+    
+    public Vivienda (int OID,int cap_max, int tam_x, int tam_y,int precio){
+        super(OID);
+        this.Tamano_x = tam_x;
+        this.Tamano_y = tam_y;
+        this.Precio = precio;
+        this.capacidad_maxima = cap_max;
+    }
+    
+    ////ModificdoraS////
+    
+    public void Setcap_max(int cap_max){
+        this.capacidad_maxima = cap_max;
+    }
+    
+    public void setTamaño(int x,int y){
+        this.Tamano_x=x;
+        this.Tamano_y=y;
+    }
+    
+    
+    ///Consultoras/////
+    
+    public int Getcap_max(){
+        return this.capacidad_maxima;
+    }
+    
+    public int getTamanoX(){
+        return this.Tamano_x;
+    }
+    
+    public int getTamanoY(){
+        return this.Tamano_y;
+    }
+}
