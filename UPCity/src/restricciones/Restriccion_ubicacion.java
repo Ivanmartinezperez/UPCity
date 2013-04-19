2ab9c9a383f5f71e97ee8438a9bc48206f85db21
ron <ron@ron-PC>
2013-04-19 13:45:07 +0200

    descripcion de las funciones

--- UPCity/src/restricciones/Restriccion_ubicacion.java
+++ UPCity/src/restricciones/Restriccion_ubicacion.java
@@ -18,6 +18,14 @@ public class Restriccion_ubicacion extends Restriccion {
     
      /// CREADORA //////////
     
+    /**
+     * Creadora de restriccion de ubicacion.
+     * @param id id de la restriccion
+     * @param tipo tipo de restriccion
+     * @param OID1 id del primer elemento relacionado
+     * @param OID2 id del segundo elemento relacionado
+     * @param distancia distancia minima entre ambos elementos
+     */
     public Restriccion_ubicacion(String id, String tipo, String OID1, String OID2, int distancia){
         super(id, tipo);
         this.OID1 = OID1;
@@ -27,20 +35,35 @@ public class Restriccion_ubicacion extends Restriccion {
     
     /// MODIFICADORES //////
     
+    /**
+     * Modificadora de distancia minima
+     * @param distancia distancia minima nueva entre los elementos
+     */
     public void modificar_distancia(int distancia){
         this.distancia = distancia;
     }
     
    /// CONSULTORES ////////
-    
+    /**
+     * Consultora de distancia minima
+     * @return devuelve la distancia minima
+     */
     public int consultar_distancia(){
         return this.distancia;
     }
     
+    /**
+     * Consultora del primer elemento relacionado
+     * @return devuelve el id del primer elemento relacionado
+     */
     public String consultar_OID1(){
         return this.OID1;
     }
     
+    /**
+     * Consultora del segundo elemento relacionado
+     * @return devuelve el id del segundo elemento relacionado
+     */
     public String consultar_OID2(){
         return this.OID2;
     }
