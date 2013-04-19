1f8eebdc04f8f717256e8c44b35427d8a655b390
ron <ron@ron-PC>
2013-04-19 15:43:45 +0200

    descripcion de las funciones

--- UPCity/src/controladors/CtrlDomRestricciones.java
+++ UPCity/src/controladors/CtrlDomRestricciones.java
@@ -16,12 +16,32 @@ public class CtrlDomRestricciones {
     private TreeMap<String, Restriccion_demografica> restdemografica;
     private Pair<String,Restriccion_economica> resteconomica;
     
+    /**
+     * Creadora de la clase controlador de dominio de restricciones
+     * @param ubicacion estructura que contiene las restricciones de ubicacion
+     * @param demografica estructura que contiene las restricciones demograficas
+     */
     public CtrlDomRestricciones(TreeMap<String, Restriccion_ubicacion> ubicacion, TreeMap<String, Restriccion_demografica> demografica){
         restubicacion = ubicacion;
         restdemografica = demografica;
         
     }
     
+    /**
+     * Encargado de crear una restriccion e ingresarla en la estructura deseada
+     * @param id id de la nueva restriccion
+     * @param tipo tipo de restriccion
+     * @param OID1 se utiliza en el caso de crear restriccion de ubicacion y restriccion demografica.
+     * es el id del primer elemento en caso de ubicacion y es el id del elemento a restringir en caso demografica
+     * @param OID2 se utiliza solo en el caso de crear restriccion de ubicacion. Es el id del segundo elemento
+     * @param aux1 en caso ubicacion es la distancia, en caso demografica es habitantes, y en caso economica es cantidad
+     * de dinero para comercios
+     * @param aux2 se utiliza solo en caso de restriccion economica. Es la cantidad de dinero que se
+     * utliza en viviendas
+     * @param aux3 se utiliza solo en caso de restriccion economica. Es la cantidad de dinero que se
+     * utliza en servicios publicos
+     * @return retorna booleano si se ha hecho la creacion e insercion correctamente
+     */
     public boolean CrearRestriccion(String id, String tipo, String OID1, String OID2, int aux1, int aux2, int aux3){
         boolean ret=true;
         switch(tipo){
@@ -39,6 +59,16 @@ public class CtrlDomRestricciones {
         return ret;
     }
     
+    /**
+     * Encargado de modificar una restriccion ya creada
+     * @param id id de la restriccion que hay que modificar
+     * @param tipo tipo de la restriccion que hay que modificar
+     * @param aux1 en caso ubicacion es la distancia nueva, en caso economica es la nueva cantidad de dinero
+     * para invertir en comercios, en caso demografico es la nueva cantidad minima de habitantes
+     * @param aux2 se utiliza solo en caso economica, es la nueva cantidad de dinero para viviendas
+     * @param aux3 se utiliza solo en caso economica, es la nueva cantidad de dinero para servicios publicos
+     * @return retorna booleano si se ha hecho la modificacion correctamente
+     */
     public boolean ModificarRestriccion(String id, String tipo, int aux1, int aux2, int aux3){
         boolean ret=true;
         switch(tipo){
@@ -48,7 +78,7 @@ public class CtrlDomRestricciones {
                                 }
                                 else ret = false;
                                 break;
-            case "economica":   if (aux1 >= 0 && aux2 >= 0 && aux3 >= 0 && resteconomica.getFirst() == id){
+            case "economica":   if (aux1 >= 0 && aux2 >= 0 && aux3 >= 0 && resteconomica.getFirst().equals(id)){
                                     Restriccion_economica e = resteconomica.getSecond();
                                     e.Modificarsaldo(aux1, aux2, aux3);
                                 }
@@ -65,6 +95,12 @@ public class CtrlDomRestricciones {
             return ret;
      }
     
+    /**
+     * Encargado de eliminar una restriccion de la estructura
+     * @param id id de la restriccion que hay que eliminar
+     * @param tipo tipo de la restriccion que hay que eliminar
+     * @return retorna booleano si se ha hecho la eliminacion correctamente
+     */
     public boolean Eliminar_Restriccion(String id, String tipo){
         boolean ret = true;
         switch(tipo){
