7685ab8c9a80d9ad0550b88e06b1e9fbcaf879aa
ron <ron@ron-PC>
2013-04-19 13:54:09 +0200

    descripcion de las funciones

--- UPCity/src/restricciones/Restriccion_economica.java
+++ UPCity/src/restricciones/Restriccion_economica.java
@@ -18,6 +18,14 @@ public class Restriccion_economica extends Restriccion {
    
    //CREADORA///////
    
+   /**
+    * Creadora de restriccion economica
+    * @param id id de la restriccion
+    * @param tipo tipo de restriccion
+    * @param saldo_com cantidad de dinero para invertir en comercios
+    * @param saldo_viv cantidad de dinero para invertir en viviendas
+    * @param saldo_pub cantidad de dinero para invertir en servicios publicos
+    */
    public Restriccion_economica( String id, String tipo,int saldo_com, int saldo_viv, int saldo_pub){
        super(id, tipo);
        this.saldo = saldo_com + saldo_viv + saldo_pub;
@@ -30,6 +38,13 @@ public class Restriccion_economica extends Restriccion {
    /////////////////
    
    //MODIFICADORAS///////
+   
+   /**
+    * Modificadora de la cantidad de dinero para invertir
+    * @param saldo_com cantidad de dinero para invertir en comercios
+    * @param saldo_viv cantidad de dinero para invertir en viviendas
+    * @param saldo_pub cantidad de dinero para invertir en servicios publicos 
+    */
    public void Modificarsaldo(int saldo_com, int saldo_viv, int saldo_pub){
        this.saldo = saldo_com + saldo_viv + saldo_pub;
        this.saldos[0] = saldo_com;
@@ -37,18 +52,35 @@ public class Restriccion_economica extends Restriccion {
        this.saldos[2] = saldo_pub;
    }
    
+   /**
+    * modificar 
+    * @param id
+    * @param saldo_ind 
+    */
+   /*
    public void Modificarsaldo_indiv(int id, int saldo_ind){
        this.saldo = this.saldo - this.saldos[id];
        this.saldos[id] = saldo_ind;
        this.saldo = this.saldo + saldo_ind;
    }
+   */
    ///////////////////////
    
    //CONSULTORAS/////////
+   /**
+    * Consultora del total de dinero para invertir
+    * @return retorna el total de dinero
+    */
    public int consultar_saldo(){
        return this.saldo;
    }
    
+   /**
+    * Consultora de cantidad de dinero para invertir en un tipo de elementos
+    * precondicion: id tiene que ser entre 0 a 2. 0 siendo comercios, 1 viviendas, y 2 servicios publicos
+    * @param id id de tipo de elemento
+    * @return retorna la cantidad de dinero para el tipo de elementos deseado
+    */
    public int consultar_saldo_ind(int id){
        return this.saldos[id];
    }
