e0b03b161d230f58536708a0ee29b2d0e7868d00
Ivan Martinez <ivaninho104@gmail.com>
2013-04-25 20:25:46 +0200

    Juego de pruebas + cruel: PASADO!!

--- UPCity/src/Pruebas/MainIvan.java
+++ UPCity/src/Pruebas/MainIvan.java
@@ -25,19 +25,20 @@ public class MainIvan {
         ArrayList<Integer> elementos = new ArrayList<>();
         for(int i=1;i<=4;++i){
             elementos.add(i);
+            if(i!=4)elementos.add(i+1);
+            if(i!=1)elementos.add(i-1);
         }
-        elementos.add(1);
         
         System.out.println("Printo mi elementos\n");
-        for(int i=0;i<=4;++i){
+        for(int i=0;i<elementos.size();++i){
             System.out.println(elementos.get(i)+" ");
         }
         System.out.println("\n");
         
         System.out.println("Printo su vector de ultima posiciones visitadas\n");
         //Crea el vector de ultimas visitas, para controlar los valores del dominio(pos en el mapa) que toman los elementos
-        Pair[] lastVisited = new Pair[5];
-        for(int i=0;i<5;++i){
+        Pair[] lastVisited = new Pair[elementos.size()];
+        for(int i=0;i<elementos.size();++i){
             Pair p = new Pair(0,0);
             lastVisited[i]=p;
         }
@@ -58,27 +59,30 @@ public class MainIvan {
         
         //Creamos restricciones
         Restriccion_ubicacion res12;
-        res12 = new Restriccion_ubicacion("0","0",1,2,7);
+        res12 = new Restriccion_ubicacion("0","0",1,2,3);
         Restriccion_ubicacion res21;
-        res21 = new Restriccion_ubicacion("0","0",2,1,7);
+        res21 = new Restriccion_ubicacion("0","0",2,1,3);
         Restriccion_ubicacion res34;
-        res34 = new Restriccion_ubicacion("0","0",3,4,1);
+        res34 = new Restriccion_ubicacion("0","0",3,4,3);
         Restriccion_ubicacion res43;
-        res43 = new Restriccion_ubicacion("0","0",4,3,1);
+        res43 = new Restriccion_ubicacion("0","0",4,3,3);
         Restriccion_ubicacion res31;
-        res31 = new Restriccion_ubicacion("0","0",3,1,1);
+        res31 = new Restriccion_ubicacion("0","0",3,1,2);
         Restriccion_ubicacion res13;
-        res13 = new Restriccion_ubicacion("0","0",1,3,1);
+        res13 = new Restriccion_ubicacion("0","0",1,3,2);
         Restriccion_ubicacion res14;
-        res14 = new Restriccion_ubicacion("0","0",1,4,1);
+        res14 = new Restriccion_ubicacion("0","0",1,4,4);
         Restriccion_ubicacion res41;
-        res41 = new Restriccion_ubicacion("0","0",4,1,1);
+        res41 = new Restriccion_ubicacion("0","0",4,1,4);
+        Restriccion_ubicacion res11;
+        res11 = new Restriccion_ubicacion("0","0",1,1,3);
         
         
         //Añadimos las restricciones a su conjunto
         r0.add(res12);
         r0.add(res13);
         r0.add(res14);
+        r0.add(res11);
         r1.add(res21);
         r2.add(res34);
         r2.add(res31);
@@ -95,7 +99,7 @@ public class MainIvan {
             System.out.println(""+res.get(i).get(0).consultar_OID1()+""+res.get(i).get(0).consultar_OID2()+""+res.get(i).get(0).consultar_distancia());
         }
         
-        Plano p = new Plano(10,10);
+        Plano p = new Plano(8,8);
         
         p.pos(0, 0).modificarPar(9, 0);
         p.pos(1, 0).modificarPar(9, 0);
@@ -107,16 +111,17 @@ public class MainIvan {
         p.pos(4, 2).modificarPar(9, 0);
         p.pos(4, 3).modificarPar(9, 0);
         p.pos(4, 4).modificarPar(9, 0);
-        p.pos(9, 0).modificarPar(9, 0);
-        p.pos(9, 1).modificarPar(9, 0);
-        p.pos(9, 2).modificarPar(9, 0);
-        p.pos(9, 3).modificarPar(9, 0);
-        p.pos(9, 4).modificarPar(9, 0);
-        p.pos(9, 5).modificarPar(9, 0);
-        p.pos(9, 6).modificarPar(9, 0);
-        p.pos(9, 7).modificarPar(9, 0);
-        p.pos(9, 8).modificarPar(9, 0);
-        p.pos(9, 9).modificarPar(9, 0);
+        p.pos(5, 4).modificarPar(9, 0);
+        p.pos(6, 4).modificarPar(9, 0);
+        p.pos(7, 0).modificarPar(9, 0);
+        p.pos(7, 1).modificarPar(9, 0);
+        p.pos(7, 2).modificarPar(9, 0);
+        p.pos(7, 3).modificarPar(9, 0);
+        p.pos(7, 4).modificarPar(9, 0);
+        p.pos(7, 5).modificarPar(9, 0);
+        p.pos(7, 6).modificarPar(9, 0);
+        p.pos(7, 7).modificarPar(9, 0);
+       
    
             
         for(int i=0;i<p.tama();++i){
