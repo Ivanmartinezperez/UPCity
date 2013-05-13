a9c99874de3414ff1e5b1f1c95cb8a90a295a041
Artem Cherkashin <archy.ac@gmail.com>
2013-05-14 01:11:58 +0200

    Persistencia de Elementos hecha(menos lo de comprobar si el elemento esta en algun barrio antes de eliminarlo)

---
+++ UPCity/src/controladors/CtrlPersDisco.java
@@ -0,0 +1,115 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controladors;
+
+import java.io.*;
+import java.util.ArrayList;
+
+
+/**
+ *
+ * @author ArclorenSarth
+ */
+public class CtrlPersDisco {
+    
+    /**
+     * Creadora por defecto. Se encarga de crear la carpeta de los datos 
+     * persistentes en el directorio del programa, llamada Data, en el caso de 
+     * de que no exista.
+     */
+    public CtrlPersDisco() {
+        File Data = new File("./Data");
+        if (!Data.exists()) {
+            Data.mkdirs();
+        }
+    }
+
+    
+    /**
+     * Crea un archivo .txt dentro de la carpeta Data con el nombre nombre.
+     * @param nombre Nombre del archivo que se desea crear.
+     */
+    public void crearArchivo(String nombre){
+        File arch = new File("./Data/" + nombre + ".o");
+        if(!arch.exists()){
+            try{
+                arch.createNewFile();
+            } catch(IOException ex){
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+    
+    
+    
+   /* public int numArxius() {
+        return (int) new File("./Data/").length();
+    }
+
+    
+    public ArrayList llistaDirectori(String paraula) {
+        File f = new File("./Data/");
+        int numArxius = f.list().length;
+        String llistaArxius[] = f.list(); // llista amb els noms de tots els arxius
+        ArrayList llista = new ArrayList();
+
+        for (int i = 0; i < numArxius; ++i) {
+            if (llistaArxius[i].contains(paraula)) {
+                llista.add(llistaArxius[i]);
+            }
+        }
+
+        return llista;
+    }
+    */
+    
+    
+    /**
+     * Lectora de un archivo del disco. La funcion lee linia a linia el archivo
+     * con el nombre nombre y añade todas las linias leidas a un ArrayList.
+     * @param nombre Nombre del archivo que se quiere leer.
+     * @return Retorna un ArrayList de todas las linias leidas del archivo.
+     */
+    public ArrayList<String> leerArchivo(String nombre) {
+        ArrayList<String> datos = new ArrayList();
+        try {
+            FileReader fr = new FileReader("./Data/" + nombre + ".o");
+            BufferedReader br = new BufferedReader(fr);
+            String line;
+            while ((line = br.readLine()) != null){  
+                datos.add(line);
+            }
+            fr.close();          
+        } catch (FileNotFoundException ex) {
+            crearArchivo(nombre);
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return datos;
+    }
+    
+    
+    /**
+     * Escritora de un archivo del disco. La funcion escribe en el archivo con
+     * el nombre nombre, linia a linea, el contenido del ArrayList datos.
+     * @param nombre Nombre del archivo sobre el que se quiere escribir.
+     * @param datos ArrayList con los datos que se quieren escribir.
+     */
+    public void escribirArchivo(String nombre, ArrayList<String> datos) {
+        try {
+            FileWriter fw = new FileWriter("./Data/" + nombre + ".o");
+            PrintWriter pw = new PrintWriter(fw);
+            for(int i=0; i<datos.size(); ++i){ 
+                pw.println(datos.get(i));
+            }
+            fw.close();           
+        } catch (FileNotFoundException ex) {
+            crearArchivo(nombre);
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        
+    }
+}
--- UPCity/src/controladors/stubbedElementosGDP.java
+++ UPCity/src/controladors/stubbedElementosGDP.java
@@ -5,7 +5,7 @@
 package controladors;
 
 import java.util.TreeMap;
-import elementos.Elemento;
+import elementos.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
@@ -17,10 +17,12 @@ import java.util.Scanner;
 public class stubbedElementosGDP {
     
     private static stubbedElementosGDP INSTANCE = null;
+    CtrlPersDisco PERSDisco;
    
     
     private stubbedElementosGDP(){
-        
+        PERSDisco = new CtrlPersDisco();
+        PERSDisco.crearArchivo("Elementos");
     }
     
     private static void creaInstancia() {
@@ -45,30 +47,105 @@ public class stubbedElementosGDP {
      * parametro.
      * @param TablaElem 
      */     
-    public void leerElementos(TreeMap<String,Elemento> TE0,
-                              HashMap<Integer,ArrayList<Elemento>> HE0,
-                              TreeMap<String,Elemento> TE1,
-                              HashMap<Integer,ArrayList<Elemento>> HE1,
-                              TreeMap<String,Elemento> TE2,
-                              HashMap<Integer,ArrayList<Elemento>> HE2,
-                              TreeMap<String,Elemento> TE3,
-                              HashMap<Integer,ArrayList<Elemento>> HE3){
+    public void leerElementos(ArrayList<Elemento> Elems,
+                              ArrayList<Integer> Tipos,
+                              ArrayList<Integer> TBarrios){
         
-        System.out.println("Simulando lectura de disco...Cargando Elementos\n"
-                + "Error! Disco NOT FOUND!\n Don't be alarm, our technicals are"
-                + " working on to solve this problem just before the third "
-                + "installment.\n For now you have to keep up with temporal "
-                + "data\n"); 
-        
-        
+        ArrayList<String> elemPers = PERSDisco.leerArchivo("Elementos");
+        String[] aux;
+        int tipo = 0;
+        for(int i=0; i<elemPers.size(); ++i){
+            aux = elemPers.get(i).split("\t");
+            Elems.add(crearElem(aux));
+            Tipos.add(Integer.parseInt(aux[3]));
+            TBarrios.add(Integer.parseInt(aux[4]));
+        }
     }
-        
+    
+    
+    private Elemento crearElem(String[] elemStr){
+        Elemento e = null;
+        switch(elemStr[3]){
+            case "1" : 
+                Vivienda v = new Vivienda(Integer.parseInt(elemStr[0]),
+                                          Integer.parseInt(elemStr[6]),
+                                          Integer.parseInt(elemStr[7]),
+                                          Integer.parseInt(elemStr[8]),
+                                          Integer.parseInt(elemStr[5]),
+                                          Integer.parseInt(elemStr[4]));
+                v.setNom(elemStr[1]);
+                v.setDescrpcio(elemStr[2]);
+                e = v;
+                break;
+            case "2" : 
+                Publico p = new Publico(Integer.parseInt(elemStr[0]),
+                                        Integer.parseInt(elemStr[6]),
+                                        Integer.parseInt(elemStr[7]),
+                                        Integer.parseInt(elemStr[8]),
+                                        Integer.parseInt(elemStr[9]),
+                                        Integer.parseInt(elemStr[5]),
+                                        Integer.parseInt(elemStr[4]));
+                p.setNom(elemStr[1]);
+                p.setDescrpcio(elemStr[2]);
+                e = p;
+                break;
+            case "3" : 
+                Comercio c = new Comercio(Integer.parseInt(elemStr[0]),
+                                          Integer.parseInt(elemStr[6]),
+                                          Integer.parseInt(elemStr[7]),
+                                          Integer.parseInt(elemStr[8]),
+                                          Integer.parseInt(elemStr[5]),
+                                          Integer.parseInt(elemStr[4]));
+                c.setNom(elemStr[1]);
+                c.setDescrpcio(elemStr[2]);
+                e = c;
+                break;
+                
+            }
+        return e;
+    }
+    
+    
+    private String transElemString(Elemento e){
+        String line = String.valueOf(e.getId());
+        line = line.concat("\t" + e.getNom());
+        line = line.concat("\t" + e.getDescripcio());
+        if(e instanceof Vivienda){
+            Vivienda v = (Vivienda) e;
+            line = line.concat("\t" + "1");
+            line = line.concat("\t" + String.valueOf(v.getTBarrio()));
+            line = line.concat("\t" + String.valueOf(v.getPrecio()));
+            line = line.concat("\t" + String.valueOf(v.Getcap_max()));
+            line = line.concat("\t" + String.valueOf(v.getTamanoX()));
+            line = line.concat("\t" + String.valueOf(v.getTamanoY()));
+        }
+        else if(e instanceof Publico){
+            Publico p = (Publico) e;
+            line = line.concat("\t" + "2");
+            line = line.concat("\t" + String.valueOf(p.getTBarrio()));
+            line = line.concat("\t" + String.valueOf(p.getPrecio()));
+            line = line.concat("\t" + String.valueOf(p.Gettipo()));
+            line = line.concat("\t" + String.valueOf(p.Getcapacidad_serv()));
+            line = line.concat("\t" + String.valueOf(p.getTamanoX()));
+            line = line.concat("\t" + String.valueOf(p.getTamanoY()));
+        }
+        else {
+            Comercio c = (Comercio) e;
+            line = line.concat("\t" + "3");
+            line = line.concat("\t" + String.valueOf(c.getTBarrio()));
+            line = line.concat("\t" + String.valueOf(c.getPrecio()));
+            line = line.concat("\t" + String.valueOf(c.getCapacidad()));
+            line = line.concat("\t" + String.valueOf(c.getTamanoX()));
+            line = line.concat("\t" + String.valueOf(c.getTamanoY()));
+            
+        }
+        return line;
+    }
     
     public void escribirElemento(Elemento e){
-        System.out.println("Simulando escritura en disco...Guardando Elemento\n"
-                + "Error! Disco NOT FOUND!\n Don't be alarm, our technicals are"
-                + " working on to solve this problem just before the third "
-                + "installment\n");
+        ArrayList<String> elemPers = PERSDisco.leerArchivo("Elementos");
+        elemPers.add(transElemString(e));
+        PERSDisco.escribirArchivo("Elementos", elemPers);
     }
         
     
@@ -84,10 +161,17 @@ public class stubbedElementosGDP {
     
     
     public void eliminarElemDisco(String Elem){
-        System.out.println("Simulando escritura en disco...Eliminando Elemento\n"
-                + "Error! Disco NOT FOUND!\n Don't be alarm, our technicals are"
-                + " working on to solve this problem just before the third "
-                + "installment\n");
+        ArrayList<String> elemPers = PERSDisco.leerArchivo("Elementos");
+        String[] aux;
+        boolean b=false;
+        for(int i=0; i<elemPers.size() && b==false; ++i){
+            aux = elemPers.get(i).split("\t");
+            if(aux[1].equals(Elem)){
+                b = true;
+                elemPers.remove(i);
+            }
+        }
+        PERSDisco.escribirArchivo("Elementos", elemPers);
     }
     
 }
